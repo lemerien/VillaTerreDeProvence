3df1d108b719f81c24ba54c2322c47cf93262905
lemerien <laurentlemerien@gmail.com>
2024-12-16 19:14:35 +0100

    site ok en dev mise page et images test2

--- src/components/gallery/ImageGallerySuiteParentale.jsx
+++ src/components/gallery/ImageGallerySuiteParentale.jsx
@@ -4,20 +4,20 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const images = [
     {
-    original: '../src/components/shared/Photos/Bedrooms/2 - Parental Suite.jpg',
-    thumbnail: '../src/components/shared/Photos/Bedrooms/2 - Parental Suite.jpg',
+    original: './src/components/shared/Photos/Bedrooms/2 - Parental Suite.jpg',
+    thumbnail: './src/components/shared/Photos/Bedrooms/2 - Parental Suite.jpg',
     description: 'Dispose de sa propre salle de bain, un grand dressing et une vue directe sur le jardin',
     originalTitle: 'Suite Parentale'
   },
   {
-    original: '../src/components/shared/Photos/Bedrooms/4 - Bathroom Parental Suite.jpg',
-    thumbnail: '../src/components/shared/Photos/Bedrooms/4 - Bathroom Parental Suite.jpg',
+    original: './src/components/shared/Photos/Bedrooms/4 - Bathroom Parental Suite.jpg',
+    thumbnail: './src/components/shared/Photos/Bedrooms/4 - Bathroom Parental Suite.jpg',
     description: 'Dans un style exotique avec des bois chauds, deux vasques et une grande douche',
     originalTitle: 'Salle de bain Suite Parentale'
   },
   {
-    original: '../src/components/shared/Photos/Bedrooms/3 - Parental Suite.jpg',
-    thumbnail: '../src/components/shared/Photos/Bedrooms/3 - Parental Suite.jpg',
+    original: './src/components/shared/Photos/Bedrooms/3 - Parental Suite.jpg',
+    thumbnail: './src/components/shared/Photos/Bedrooms/3 - Parental Suite.jpg',
     description: 'Dispose de sa propre salle de bain, un grand dressing et une vue directe sur le jardin',
     originalTitle: 'Suite Parentale'
   }
